fd77892db24f32ed9b1914b6eb6ca208d049c394
vishwas-trivedi <vishwas.trivedi2012@gmail.com>
2019-11-15 17:54:13 +0530

    added new and create actions with white listing

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    # White listing of parameters
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+
+  private
+    # White listing/Strong parameter
+    # Used to permit certain parameter in certain action
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end
